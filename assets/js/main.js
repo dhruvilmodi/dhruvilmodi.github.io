34f975aa140373f375ed9d4c35740eaeaec72c12
Dhruvil Modi <connect@dhruvilmodi.com>
2020-08-16 08:54:10 -0400

    changes to the main frame

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 let logo = document.querySelector("#logo");
 let welcomeText = document.querySelector("#welcomeText");
 let welcomeSubText = document.querySelector("#welcomeSubText");
+let nav = document.querySelector("nav");
 
 function welcomeanimation() {
     // logo
@@ -17,13 +18,28 @@ function welcomeanimation() {
         delay: 0.5
     });
     // subtext
-    gsap.to(welcomeSubText, {
+    gsap.fromTo(welcomeSubText, {
+        opacity: 0,
+        ease: "power2.inOut",
+    },{
         opacity: 1,
         ease: "power2.inOut",
-        delay: 1
+        duration: 1,
+        delay: 1,
+        repeat: -1,
+        yoyo: true
     });
 }
 
 $(document).ready(function(){
     welcomeanimation();
+
+    // on scroll animation
+    window.addEventListener("scroll", function () {
+        if (document.documentElement.scrollTop > 250) {
+            gsap.to(nav, 0.3, {display:"flex", opacity: 1, duration:0.3, ease:"power2.in", yoyo:true});
+        }else{
+            gsap.to(nav, 0.3, {display:"none", opacity: 0, duration:0.3, ease:"power2.out", yoyo:true});
+        }
+    });
 });
