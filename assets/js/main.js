37569bd1ec29391cfeb80bbab245400a4a1adb18
Dhruvil Modi <dhruvilmodi555@gmail.com>
2020-09-06 17:28:02 -0400

    minimized the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,161 +51,62 @@ function handleComplete(evt,comp) {
 /* ---------------- submarine animation ---------------- */
 
 function subAnimation() {
-    gsap.to(".subMarine", 1, {
-        opacity: 1,
-        delay: 1
-    });
-    gsap.fromTo(".subMarine", 4, {
-        y: 10,
-        rotate: 5
-    },{
-        y: -10,
-        rotate: -5,
-        yoyo:true,
-        repeat: -1,
-        ease: "power1.inOut"
-    });
+    gsap.to(".subMarine", 1, {opacity: 1, delay: 1});
+    gsap.fromTo(".subMarine", 4, {y: 10, rotate: 5}, {y: -10, rotate: -5, yoyo:true, repeat: -1, ease: "power1.inOut"});
 }
 
 /* ---------------- divetext animation ---------------- */
 
 function diveTextAnimation() {
-    gsap.timeline().fromTo(diveButton, 1, {opacity: 0.2},{opacity: 1, yoyo:true, repeat: -1, ease: "power1.inOut"});
+	var diveTl = gsap.timeline();
+    diveTl.fromTo(diveButton, 1, {opacity: 0.2},{opacity: 1, yoyo:true, repeat: -1, ease: "power1.inOut"});
 }
 
 /* ---------------- fish animation ---------------- */
 
 function fishAnim() {
-	$(".fish1About").fadeIn("fast");
-	$(".fish2About").fadeIn("fast");
-	$(".fish3About").fadeIn("fast");
-	$(".fish4About").fadeIn("fast");
-	$(".fish5About").fadeIn("fast");
-
-	// fish1
-	gsap.fromTo(".fish1About", {
-		x: "100vw"
-	},{
-		x: -700,
-		ease: "power4.in",
-		duration: 6,
-		repeat: -1,
-		repeatDelay: 3
-	});
-
-	// fish2
-	gsap.fromTo(".fish2About", {
-		x: "100vw"
-	},{
-		x: -700,
-		ease: "power4.in",
-		duration: 6,
-		repeat: -1,
-		repeatDelay: 3
-	});
-
-	// fish3
-	gsap.fromTo(".fish3About", {
-		x: "100vw"
-	},{
-		x: -700,
-		ease: "power4.in",
-		duration: 6,
-		repeat: -1,
-		repeatDelay: 3
-	});
-
-	// fish4
-	gsap.fromTo(".fish4About", {
-		x: "100vw"
-	},{
-		x: -700,
-		ease: "power4.in",
-		duration: 6,
-		repeat: -1,
-		repeatDelay: 3
-	});
-
-	// fish5
-	gsap.fromTo(".fish5About", {
-		x: "100vw"
-	},{
-		x: -800,
-		ease: "power4.in",
-		duration: 6,
-		repeat: -1,
-		repeatDelay: 3
-	});
+	$(".aboutFishes").fadeIn("fast");
+	
+	var fishTl = gsap.timeline();
+	fishTl.fromTo(".fish1About", {x: "100vw"}, {x: -700, ease: "power4.in", duration: 6, repeat: -1, repeatDelay: 3})
+		.fromTo(".fish2About", {x: "100vw"}, {x: -700, ease: "power4.in", duration: 6, repeat: -1, repeatDelay: 3})
+		.fromTo(".fish3About", {x: "100vw"}, {x: -700, ease: "power4.in", duration: 6, repeat: -1, repeatDelay: 3})
+		.fromTo(".fish4About", {x: "100vw"}, {x: -700, ease: "power4.in", duration: 6, repeat: -1, repeatDelay: 3})
+		.fromTo(".fish5About", {x: "100vw"}, {x: -800, ease: "power4.in", duration: 6, repeat: -1, repeatDelay: 3});
 }
 
 /* ---------------- about animation ---------------- */
 
 function aboutAnim() {
-	gsap.timeline().to(".about", {opacity: 1, stagger: 0.2, duration: 1.5, ease: "back"})
+	var aboutTl = gsap.timeline();
+	aboutTl.to(".about", {opacity: 1, stagger: 0.2, duration: 1.5, ease: "back"})
 }
 
 /* ---------------- jellyFish animation ---------------- */
 
 function jellyAnimation() {
-	// $(".jellyFish1").fadeIn("fast");
 
-	// jelly1
-	gsap.fromTo(".jellyFish1", {
-		y: 10,
-		x: -5,
-		rotate: 5
-	},{
-		y: -10,
-		x: 5,
-		rotate: -5,
-		ease: "power1.inOut",
-		duration: 3,
-		yoyo: true,
-		repeat: -1,
-	});
-
-	// jelly2
-	gsap.fromTo(".jellyFish2", {
-		y: -10,
-		x: 4,
-		rotate: 4
-	},{
-		y: 10,
-		x: -5,
-		rotate: -5,
-		ease: "power1.inOut",
-		duration: 3,
-		yoyo: true,
-		repeat: -1,
-	});
-
-	// jelly3
-	gsap.fromTo(".jellyFish3", {
-		y: -5,
-		x: 2,
-	},{
-		y: 5,
-		x: -2,
-		ease: "power2.inOut",
-		duration: 5,
-		yoyo: true,
-		repeat: -1,
-	});
+	var jellyTl = gsap.timeline();
+	jellyTl.fromTo(".jellyFish1", {y: 10, x: -5, rotate: 5}, {y: -10, x: 5, rotate: -5, ease: "power1.inOut", duration: 3, yoyo: true, repeat: -1})
+		.fromTo(".jellyFish2", {y: -10, x: 4, rotate: 4},{y: 10, x: -5, rotate: -5, ease: "power1.inOut", duration: 3, yoyo: true, repeat: -1})
+		.fromTo(".jellyFish3", {y: -5, x: 2}, {y: 5, x: -2, ease: "power2.inOut", duration: 5, yoyo: true, repeat: -1});
 
 }
 
 /* ---------------- skillsPage animation ---------------- */
 
 function skillsAnimation() {
-	gsap.timeline().to(".skills", {opacity: 1, stagger: 0.1, duration: 1.5, ease: "back"});
-	gsap.fromTo(".skillIcons", {rotate: 2},{rotate: -2, yoyo: true, ease:"power1.inOut", repeat: -1, duration: 1});
+	var skillsTl = gsap.timeline();
+	skillsTl.to(".skills", {opacity: 1, stagger: 0.1, duration: 1.5, ease: "back"})
+		.fromTo(".skillIcons", {rotate: 2},{rotate: -2, yoyo: true, ease:"power1.inOut", repeat: -1, duration: 1});
 }
 
 /* ---------------- shark animation ---------------- */
 
 function sharkAnimation() {
-	gsap.fromTo(".shark1", {x: "0"},{x:"-130vw", repeat: -1, repeatDelay: 2, ease: "power1.in", duration: 12});
-	gsap.fromTo(".shark2", {x: "0"},{x:"-120vw", repeat: -1, repeatDelay: 2, ease: "power1.in", duration: 12});
+	var sharksTl = gsap.timeline();
+	sharksTl.fromTo(".shark1", {x: "0"},{x:"-130vw", repeat: -1, repeatDelay: 2, ease: "power1.in", duration: 12})
+		.fromTo(".shark2", {x: "0"},{x:"-120vw", repeat: -1, repeatDelay: 2, ease: "power1.in", duration: 12});
 }
 
 
@@ -213,15 +114,11 @@ function sharkAnimation() {
 
 $(document).ready(function(){
 
+	gsap.registerPlugin(ScrollToPlugin);
+
 	var aboutAnimated = false;
 
-	$(".fish1About").fadeOut("fast");
-	$(".fish2About").fadeOut("fast");
-	$(".fish3About").fadeOut("fast");
-	$(".fish4About").fadeOut("fast");
-	$(".fish5About").fadeOut("fast");
-
-	// $(".jellyFish1").fadeOut("fast");
+	$(".aboutFishes").fadeOut("fast");
 
     heroPageAnimation();
     subAnimation();
@@ -229,6 +126,11 @@ $(document).ready(function(){
 	fishAnim();
 	jellyAnimation();
 	sharkAnimation();
+
+	$(document).on('click', diveButton, function() {
+		gsap.registerPlugin(ScrollToPlugin);
+		gsap.to(window, {duration: 2, scrollTo: "#aboutPage"});
+	});
 	
 
 	$(document).on('scroll', function() {
